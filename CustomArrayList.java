c2e20106b011ec5d12111b2418fabdf69d470e14
JJisak <76518907+JJisak@users.noreply.github.com>
2023-04-30 12:34:05 -0400

    Update CustomArrayList.java

--- CustomArrayList.java
+++ CustomArrayList.java
@@ -1,29 +1,28 @@
-
 public class CustomArrayList<T> implements CustomList<T> {
-	Object[] items = new Object[10];
+    private Object[] items = new Object[10];
+    private int size = 0;
 
-	@Override
-	public boolean add(T item) {
-		// TODO Auto-generated method stub
-		for (int i = 0; i < items.length; i++) {
-			if (items[i] == null) {
-				items[i] = item;
-				return true;
-			}
-		}
-		return false;
-	}
+    @Override
+    public boolean add(T item) {
+        if (size == items.length) {
+            Object[] newItems = new Object[2 * size];
+            System.arraycopy(items, 0, newItems, 0, size);
+            items = newItems;
+        }
+        items[size++] = item;
+        return true;
+    }
 
-	@Override
-	public int getSize() {
-		// TODO Auto-generated method stub
-		return items.length;
-	}
+    @Override
+    public int getSize() {
+        return size;
+    }
 
-	@Override
-	public T get(int index) {
-		// TODO Auto-generated method stub
-		return items.indexOf(index);
-	}
-	
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (T) items[index];
+    }
 }
